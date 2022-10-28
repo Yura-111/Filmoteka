231e25a9f332bc43eefc4f862aec04fb0c60da5d
Yura-111 <104792276+Yura-111@users.noreply.github.com>
2022-10-28 19:05:19 +0000

    Deploying to gh-pages from @ Yura-111/filmoteka-project@f3914ae4332f1d3abed210d85d325d7d3f3375ad 🚀

---
+++ index.3cbbad8b.js
@@ -0,0 +1,2 @@
+({watchedBtn:document.querySelector(".watched"),queueBtn:document.querySelector(".queue"),adwBtn:document.querySelector(".adw"),atqBtn:document.querySelector(".atq")}).adwBtn.addEventListener("click",(function(){return searchQuery="",console.log({name:name})}));
+//# sourceMappingURL=index.3cbbad8b.js.map
---
+++ index.52e09528.js
@@ -0,0 +1,2 @@
+var refs={watchedBtn:document.querySelector(".watched"),queueBtn:document.querySelector(".queue"),adwBtn:document.querySelector(".adw"),atqBtn:document.querySelector(".atq")};function fetchFilms(){searchQuery="";"https://api.themoviedb.org/3/movie/".concat(searchQuery,"?api_key=e862719ae3fd0863df1748cb2452c82b&language=en-US");return console.log({name:name})}refs.adwBtn.addEventListener("click",fetchFilms);
+//# sourceMappingURL=index.52e09528.js.map
