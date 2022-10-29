f5edeff87e0ccf957f7c32b240cbe552c0bc439f
Yura <104792276+Yura-111@users.noreply.github.com>
2022-10-30 02:36:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,32 +18,76 @@ const refs = {
     }
     
     const LOCAL_STORAGE_KEY = 'films';
-    
+    let parseFilm=[];
+    let deletePosition=-1; 
+
+
     const saveFilm = localStorage.getItem(LOCAL_STORAGE_KEY);
         if (!saveFilm)  {
-        localStorage.setItem (LOCAL_STORAGE_KEY, JSON.stringify([]));
-        
+        localStorage.setItem (LOCAL_STORAGE_KEY, JSON.stringify([])); 
         };            
 
-    refs.adwBtn.addEventListener('click', () => {
-        const saveFilm = localStorage.getItem(LOCAL_STORAGE_KEY);
-        // console.log(saveFilm.length);
-        try {
-            const parseFilm = JSON.parse(saveFilm);
-            console.log(parseFilm);
-        } catch (error) {
-            console.log(error);
-            // const parseFilm = JSON.parse(saveFilm);
-            // console.log(parseFilm);
-        }
-        if (!saveFilm) {
-            localStorage.setItem (LOCAL_STORAGE_KEY, JSON.stringify(films));
-            refs.adwBtn.innerText = 'Remove';
-            return 
-        } else {
-            localStorage.removeItem(LOCAL_STORAGE_KEY);
-            refs.adwBtn.innerText = 'add to Watched';
-            return 
-        }
-        })
 
+    
+        function statusBtn(object){
+            let arrayFilms=[]
+            try {
+                arrayFilms = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)); 
+            } catch (error) {
+                console.log(error);            
+            }
+            if (!arrayFilms.length){
+                console.log("Список фільмів збережених у памяті пустий кнопка add to watch");
+                refs.adwBtn.innerText = 'ADD TO WATCHED';
+                return 0;
+            } 
+            else
+            {
+            const resultSearch = arrayFilms.findIndex(option => option.id === object.id)
+                if (resultSearch>-1){
+                    console.log("Фільм знаходиться в збережених у памяті кнопка remove під номером - ",resultSearch);
+                refs.adwBtn.innerText = 'REMOVE';
+                return resultSearch;
+                } else
+                {
+                    console.log("Фільма немає в збережених у памяті кнопка add to watch");
+                    refs.adwBtn.innerText = 'ADD TO WATCHED';
+                return -1;
+                }
+
+            }
+        }
+
+    function addFilm(object){
+        try {
+            const arrayF = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+            arrayF.push(object)
+            localStorage.setItem (LOCAL_STORAGE_KEY, JSON.stringify(arrayF));  
+            console.log('фільм додано')
+            refs.adwBtn.innerText = 'REMOVE';
+        } catch (error) {
+            console.log(error);            
+        }
+        }
+    function removeFilm(deleteP){
+        try {
+            const arrayF = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+            arrayF.splice(deleteP,1)
+            localStorage.setItem (LOCAL_STORAGE_KEY, JSON.stringify(arrayF));
+            console.log('фільм видалено')
+            refs.adwBtn.innerText = 'ADD TO WATCHED';   
+        } catch (error) {
+            console.log(error);            
+        }
+        }
+
+
+        const b = Math.floor(Math.random() * 7);
+        console.log('Рандомний фільм', films[b]);
+        deletePosition = statusBtn(films[b]);
+
+    refs.adwBtn.addEventListener('click', () => {
+    const teksT = refs.adwBtn.innerText;
+    console.log(teksT);
+    (teksT==='ADD TO WATCHED') ? addFilm(films[b]) : removeFilm(deletePosition);
+    })
