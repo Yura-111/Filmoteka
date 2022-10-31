6eb9f59e5dedc39bb3559dbef68ee0151389f711
Yura <104792276+Yura-111@users.noreply.github.com>
2022-10-31 02:25:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,10 +39,6 @@ const refs = {
             console.log(error);            
         }
 
-    
-        function statusBtn(object) {
-            checkВutton(object, LOCAL_STORAGE_KEY, refs.adwBtn,'WATCHED')
-        } 
         function checkВutton(object, locstorkey, refsBtn, tekst) {
             let arrayFilms = []
             try {
@@ -109,8 +105,8 @@ const refs = {
             console.log(error);            
     }}
     refs.watchedBtn.addEventListener('click', () => {
-        massiv(LOCAL_STORAGE_KEY)
         // massiv(LOCAL_STORAGE_KEY)
+        console.log(massiv(LOCAL_STORAGE_KEY)); 
     })
 
 
@@ -145,6 +141,6 @@ const refs = {
         const b = Math.floor(Math.random() * 7);
         console.log('Рандомний фільм', films[b]);
         deletePosition = checkВutton(films[b], LOCAL_STORAGE_KEY, refs.adwBtn,'WATCHED');
-        deletePositionQ = checkВutton(films[b], LOCAL_STORAGE_KEY_QUEUE,  refs.atqBtn,'QUEUE');
+        deletePositionQ = checkВutton(films[b], LOCAL_STORAGE_KEY_QUEUE, refs.atqBtn,'QUEUE');
         
     })
